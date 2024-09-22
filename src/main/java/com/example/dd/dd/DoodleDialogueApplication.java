e2098b1c6e4b4033ae096e519785ca110d092cfd
rashmi1702 <rrashmi.17official.com>
2024-09-22 11:38:45 +0530

    first commit

---
+++ src/main/java/com/example/dd/dd/DoodleDialogueApplication.java
@@ -0,0 +1,13 @@
+package com.example.dd.dd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoodleDialogueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoodleDialogueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dd/dd/DoodleDialogueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dd.dd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoodleDialogueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
